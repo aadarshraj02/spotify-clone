405b57ced99ae5aca30bdc9bc025692fd0752033
adarsh02 <aadarshraazz007@gmail.com>
2025-01-24 18:30:41 +0530

    initial commit installed tailwind and shadcn ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-800">hello</h1>
+      <Button variant={"outline"}>Check</Button>
+    </>
+  );
+}
+
+export default App;
