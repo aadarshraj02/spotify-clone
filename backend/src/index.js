28d6f79b3bb7f5bb9cc6a027a3bba8d6d597675c
adarsh02 <aadarshraazz007@gmail.com>
2025-01-24 21:15:44 +0530

    tested the userRoute

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoute.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running");
