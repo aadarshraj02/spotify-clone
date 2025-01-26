783ce587652d25767d3bd82f4d93a93c86d7b3b9
adarsh02 <aadarshraazz007@gmail.com>
2025-01-26 18:29:19 +0530

    added routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoute.js";
+import adminRoutes from "./routes/adminRoute.js";
+import authRoutes from "./routes/songRoute.js";
+import songRoutes from "./routes/songRoute.js";
+import albumRoutes from "./routes/albumRoute.js";
+import statRoutes from "./routes/statRoute.js";
 import cors from "cors";
 
 dotenv.config();
@@ -14,6 +19,11 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running");
