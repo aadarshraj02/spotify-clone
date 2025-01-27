91fe9e72b75f3a5d0ca1501250a5f593a4901bb3
adarsh02 <aadarshraazz007@gmail.com>
2025-01-27 22:13:28 +0530

    added function to create new song

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,3 +1,40 @@
-export const createSong = (req,res)=>{
-    
-}
+import { Song } from "../models/song.model.js";
+import { Album } from "../models/album.model.js";
+
+export const createSong = async (req, res) => {
+  try {
+    if (!req.files || !req.files.audioFile || !req.files.imageFile) {
+      return res.status(400).json({
+        message: "Please upload all the required files.",
+      });
+    }
+
+    const { title, artist, albumId, duration } = req.body;
+    const audioFile = req.files.audioFile;
+    const imageFile = req.files.imageFile;
+
+    const song = new Song({
+      title,
+      artist,
+      audioUrl,
+      imageUrl,
+      duration,
+      albumId: albumId || null,
+    });
+
+    await song.save();
+    if (albumId) {
+      await Album.findByIdAndUpdate(albumId, {
+        $push: {
+          songs: song._id,
+        },
+      });
+    }
+    res.status(201).json(song);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server Error",
+      error,
+    });
+  }
+};
