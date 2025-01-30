7c62be9f23dfdb48b73c8bc9d8e089c05aff264b
adarsh02 <aadarshraazz007@gmail.com>
2025-01-30 17:29:40 +0530

    added route to create and delete album

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { createSong, deleteSong } from "../controller/admin.controller.js";
+import {
+  createAlbum,
+  createSong,
+  deleteAlbum,
+  deleteSong,
+} from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/songs", protectRoute, requireAdmin, createSong);
 router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
+
+router.post("/album", protectRoute, requireAdmin, createAlbum);
+router.delete("/album/:id", protectRoute, requireAdmin, deleteAlbum);
 
 export default router;
