d2faff200086587125f6daf49b92d893f6b298d0
adarsh02 <aadarshraazz007@gmail.com>
2025-01-30 16:39:08 +0530

    added function to handle upload to cloudinary

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,5 +1,18 @@
 import { Song } from "../models/song.model.js";
 import { Album } from "../models/album.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
+const uploadToCloudinary = async (file) => {
+  try {
+    const result = await cloudinary.uploader.upload(file.tempFilePath, {
+      resource_type: "auto",
+    });
+    return result.secure_url;
+  } catch (error) {
+    console.log("Error in uploading file");
+    throw new Error(error);
+  }
+};
 
 export const createSong = async (req, res, next) => {
   try {
@@ -12,6 +25,9 @@ export const createSong = async (req, res, next) => {
     const { title, artist, albumId, duration } = req.body;
     const audioFile = req.files.audioFile;
     const imageFile = req.files.imageFile;
+
+    const audioUrl = await uploadToCloudinary(audioFile);
+    const imageUrl = await uploadToCloudinary(imageFile);
 
     const song = new Song({
       title,
