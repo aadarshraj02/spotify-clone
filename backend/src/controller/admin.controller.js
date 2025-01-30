35568b0a5a012ea706cbde9552c00471acdbbd42
adarsh02 <aadarshraazz007@gmail.com>
2025-01-30 16:59:35 +0530

    added function to delete song

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -52,6 +52,26 @@ export const createSong = async (req, res, next) => {
   }
 };
 
-export const deleteSong = async(req,res,next)=>{
-  
-}
+export const deleteSong = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const song = await Song.findById(id);
+
+    if (song.albumId) {
+      await Album.findByIdAndUpdate(song.albumId, {
+        $pull: {
+          songs: song._id,
+        },
+      });
+    }
+
+    await Song.findByIdAndDelete(id);
+
+    res.status(200).json({
+      message: "Song deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
