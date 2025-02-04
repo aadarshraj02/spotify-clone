2c379221c3523ae4908bbee352e48a2ad5cff4fa
adarsh02 <aadarshraazz007@gmail.com>
2025-02-04 13:22:20 +0530

    added function to get all album

---
+++ backend/src/controller/album.controller.js
@@ -0,0 +1,12 @@
+import { Album } from "../models/album.model";
+
+export const getAllAlbums = async (req, res, next) => {
+  try {
+    const albums = await Album.find();
+    res.status(200).json(albums);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAlbumsById = async (req, res, next) => {};
