b87dd9b01934144f4f8c3de05218c4798f566696
adarsh02 <aadarshraazz007@gmail.com>
2025-02-06 18:13:14 +0530

    added signbout button

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,4 +1,4 @@
-import { SignedOut } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import SignInOAuthButtons from "./SignInOAuthButtons";
@@ -17,6 +17,9 @@ const Topbar = () => {
           </Link>
         )}
 
+        <SignedIn>
+          <SignOutButton />
+        </SignedIn>
         <SignedOut>
           <SignInOAuthButtons />
         </SignedOut>
