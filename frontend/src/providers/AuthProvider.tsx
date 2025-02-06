a85d5921d6c2a60fecb68e5f95efed79fb47ca0a
adarsh02 <aadarshraazz007@gmail.com>
2025-02-06 16:07:05 +0530

    added loading screen

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,6 +1,7 @@
 import { axiosInstance } from "@/lib/axios";
 import { useAuth } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
+import { Loader } from "lucide-react";
 
 const updateApiToken = (token: string | null) => {
   if (token) {
@@ -10,8 +11,8 @@ const updateApiToken = (token: string | null) => {
   }
 };
 
-const AuthProvider = () => {
-  const { getToken, userId } = useAuth();
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { getToken } = useAuth();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -30,12 +31,12 @@ const AuthProvider = () => {
     initAuth();
   }, [getToken]);
 
-  if (loading) return
-   <div className="h-screen w-full flex items-center justify-center">
-
+  if (loading) return;
+  <div className="h-screen w-full flex items-center justify-center">
+    <Loader className="size-8 text-emerald-500 animate-spin" />
   </div>;
 
-  return <div>AuthProvider</div>;
+  return <>{children}</>;
 };
 
 export default AuthProvider;
