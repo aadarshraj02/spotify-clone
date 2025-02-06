fb31ebb2a931e0912a0532b2a2e8ab3fdbf4297d
adarsh02 <aadarshraazz007@gmail.com>
2025-02-06 12:49:38 +0530

    added route for homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
     <>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   );
 }
