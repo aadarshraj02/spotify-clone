adf4c5f811f3ec089340fd7ed6964245aa33385e
adarsh02 <aadarshraazz007@gmail.com>
2025-02-06 12:52:14 +0530

    added route for authcallback page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth/AuthCallbackPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path = "/auth-callback" element ={AuthCallbackPage} />
       </Routes>
     </>
   );
