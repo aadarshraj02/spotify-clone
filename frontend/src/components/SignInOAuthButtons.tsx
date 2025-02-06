248c65f2db718acae177f185aa04600e32878317
adarsh02 <aadarshraazz007@gmail.com>
2025-02-06 18:06:07 +0530

    Added signin from google

--- frontend/src/components/SignInOAuthButtons.tsx
+++ frontend/src/components/SignInOAuthButtons.tsx
@@ -1,5 +1,30 @@
+import { useSignIn } from "@clerk/clerk-react";
+import { Button } from "./ui/button";
+
 const SignInOAuthButtons = () => {
-  return <div>SignInOAuthButtons</div>;
+  const { signIn, isLoaded } = useSignIn();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  const signInWithGoogle = async () => {
+    signIn.authenticateWithRedirect({
+      strategy: "oauth_google",
+      redirectUrl: "/sso-callback",
+      redirectUrlComplete: "/auth-callback",
+    });
+  };
+
+  return (
+    <Button
+      variant={"secondary"}
+      className="w-full text-white border-zinc-200 h-11"
+      onClick={signInWithGoogle}
+    >
+      Continue with Google
+    </Button>
+  );
 };
 
 export default SignInOAuthButtons;
