f5c4c0a0fa96dbddd9c0a501e2da0ff0445ede80
adarsh02 <aadarshraazz007@gmail.com>
2025-02-10 12:43:10 +0530

    updated mainLayout in homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,24 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
         <Route
-					path='/sso-callback'
-					element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
-				/>
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signUpForceRedirectUrl={"/auth-callback"}
+            />
+          }
+        />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
       </Routes>
     </>
   );
