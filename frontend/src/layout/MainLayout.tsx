d6a986b1be589a490f9757517b005ed6bc9049de
adarsh02 <aadarshraazz007@gmail.com>
2025-02-10 13:44:14 +0530

    added resizable layout

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,5 +1,46 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Outlet } from "react-router-dom";
+
 const MainLayout = () => {
-  return <div className="h-screen bg-black text-white flex flex-col">MainLayout</div>;
+  const isMobile = false;
+
+  return (
+    <div className="h-screen bg-black text-white flex flex-col">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="flex flex-1 h-full overflow-hidden p-2"
+      >
+        <ResizablePanel
+          defaultSize={200}
+          minSize={isMobile ? 0 : 10}
+          maxSize={30}
+        >
+          Left side panel
+        </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors hover:bg-zinc-800" />
+
+        <ResizablePanel defaultSize={isMobile ? 80 : 60}>
+          <Outlet />
+        </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors hover:bg-zinc-800" />
+
+        <ResizablePanel
+          defaultSize={20}
+          minSize={0}
+          maxSize={25}
+          collapsedSize={0}
+        >
+          Friends List and Activity
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
 };
 
 export default MainLayout;
