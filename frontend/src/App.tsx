485dd869c1483491361489db6102a784c321f25c
adarsh02 <aadarshraazz007@gmail.com>
2025-02-10 17:00:05 +0530

    added route and imported Chat page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         />
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
         </Route>
       </Routes>
     </>
