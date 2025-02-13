51482fdc65e1b0c5cc35e47f9d9a59bf1d2e5468
adarsh02 <aadarshraazz007@gmail.com>
2025-02-13 15:12:30 +0530

    added chat an dhome section

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -1,5 +1,47 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { SignedIn } from "@clerk/clerk-react";
+import { HomeIcon, MessageCircle } from "lucide-react";
+import { Link } from "react-router-dom";
+
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  return (
+    <div className="h-full flex flex-col gap-2">
+      <div className="rounded-lg bg-zinc-900 p-4">
+        <div className="space-y-2">
+          <Link
+            to={"/"}
+            className={cn(
+              buttonVariants({
+                variant: "ghost",
+                className: "w-full justify-start text-white hover:bg-zinc-800",
+              })
+            )}
+          >
+            <HomeIcon className="size-5b mr-2" />
+            <span className="hidden md:inline">Home</span>
+          </Link>
+
+          <SignedIn>
+            <Link
+              to={"/chat"}
+              className={cn(
+                buttonVariants({
+                  variant: "ghost",
+                  className:
+                    "w-full justify-start text-white hover:bg-zinc-800",
+                })
+              )}
+            >
+              <MessageCircle className="size-5b mr-2" />
+              <span className="hidden md:inline">Messages</span>
+            </Link>
+          </SignedIn>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default LeftSidebar;
