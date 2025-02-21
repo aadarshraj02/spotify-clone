0767aa14cd466f7fd0b7d9a118ace15b3c0bdd20
adarsh02 <aadarshraazz007@gmail.com>
2025-02-21 18:19:26 +0530

    added table header updated layout

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useMusicStore } from "@/stores/useMusicStore";
+import { Clock, Play } from "lucide-react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
@@ -23,18 +25,43 @@ const AlbumPage = () => {
           ></div>
           <div className="relative z-10">
             <div className="flex p-6 gap-6 pb-8">
-                <img src={currentAlbum?.imageUrl} alt={currentAlbum?.title}
+              <img
+                src={currentAlbum?.imageUrl}
+                alt={currentAlbum?.title}
                 className="w-[240px] h-[240px] shadow-lg rounded "
-                />
-                <div className="flex flex-col justify-end">
-                    <p className="text-sm font-medium">Album</p>
-                    <h1 className="text-7xl font-bold my-4">{currentAlbum?.title}</h1>
-                    <div className="flex items-center gap-2 text-sm text-zinc-100">
-                        <span className="font-medium text-white">{currentAlbum?.artist}</span>
-                        <span>• {currentAlbum?.songs.length} songs</span>
-                        <span>• {currentAlbum?.releaseYear}</span>
-                    </div>
+              />
+              <div className="flex flex-col justify-end">
+                <p className="text-sm font-medium">Album</p>
+                <h1 className="text-7xl font-bold my-4">
+                  {currentAlbum?.title}
+                </h1>
+                <div className="flex items-center gap-2 text-sm text-zinc-100">
+                  <span className="font-medium text-white">
+                    {currentAlbum?.artist}
+                  </span>
+                  <span>• {currentAlbum?.songs.length} songs</span>
+                  <span>• {currentAlbum?.releaseYear}</span>
                 </div>
+              </div>
+            </div>
+            <div className="px-6 pb-4 flex items-center gap-6">
+              <Button
+                size="icon"
+                className="w-14 h-14 rounded-full bg-green-500 hover:bg-green-400 hover:scale-105 transition-all"
+              >
+                <Play className="h-7 w-7 text-black" />
+              </Button>
+            </div>
+            <div className="bg-black/20 backdrop:blur-sm">
+              <div className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm text-zinc-400 border-b border-white/5">
+              
+              <div>#</div>
+              <div>Title</div>
+              <div>Released Date</div>
+              <div>
+                <Clock className="w-4 h-4" />
+              </div>
+              </div>
             </div>
           </div>
         </div>
