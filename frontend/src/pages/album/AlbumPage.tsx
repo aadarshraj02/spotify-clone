36b8eb1d2d41de3b6df6210a8880687c3f391315
adarsh02 <aadarshraazz007@gmail.com>
2025-02-22 10:46:30 +0530

    added table layout for the songs

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -61,6 +61,39 @@ const AlbumPage = () => {
                   <Clock className="w-4 h-4" />
                 </div>
               </div>
+
+              <div className="px-6 ">
+                <div className="space-y-2 py-4">
+                  {currentAlbum?.songs.map((song, index) => (
+                    <div
+                      key={song?._id}
+                      className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer"
+                    >
+                      <div className="flex items-center justify-center">
+                        <span className="group-hover:hidden">{index + 1}</span>
+                        <Play className="h-4 w-4 hidden group-hover:block" />
+                      </div>
+                      <div className="flex items-center gap-3">
+                        <img
+                          src={song?.imageUrl}
+                          alt={song?.title}
+                          className="size-10"
+                        />
+                        <div>
+                          <div className="font-medium text-white">
+                            {song?.title}
+                          </div>
+                          <div>{song?.artist}</div>
+                        </div>
+                      </div>
+                      <div className="flex items-center">
+                        {song.createdAt.split("T")[0]}
+                      </div>
+                      <div className="flex items-center">{song.duration}</div>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
