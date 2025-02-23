f5350a9e614fd7f5e414f2088a4608c096fa73eb
adarsh02 <aadarshraazz007@gmail.com>
2025-02-23 14:29:45 +0530

    done with homepage

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -2,15 +2,16 @@ import Topbar from "@/components/Topbar";
 import { useMusicStore } from "@/stores/useMusicStore";
 import { useEffect } from "react";
 import FeaturedSection from "./components/FeaturedSection";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import SectionGrid from "./components/SectionGrid";
 
 const HomePage = () => {
   const {
     fetchFeaturedSongs,
     fetchMadeForYouSongs,
     fetchTrendingSongs,
-    isLoading,
     madeForYouSongs,
-    featuredSongs,
+    isLoading,
     trendingSongs,
   } = useMusicStore();
 
@@ -20,11 +21,33 @@ const HomePage = () => {
     fetchTrendingSongs();
   }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
 
+  console.log("Trending Songs:", trendingSongs);
+
   return (
-    <div className="rounded-md overflow-hidden">
+    <main className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-700 to-zinc-900">
       <Topbar />
-      <FeaturedSection/>
-    </div>
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+            Good Afternoon
+          </h1>
+          <FeaturedSection />
+
+          <div className="space-y-8">
+            <SectionGrid
+              title="Made for You"
+              songs={madeForYouSongs}
+              isLoading={isLoading}
+            />
+            <SectionGrid
+              title="Trending"
+              songs={trendingSongs}
+              isLoading={isLoading}
+            />
+          </div>
+        </div>
+      </ScrollArea>
+    </main>
   );
 };
 
