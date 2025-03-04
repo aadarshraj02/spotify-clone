f400b438a0c2c641b8b77f65a8c7fb809800137b
adarsh02 <aadarshraazz007@gmail.com>
2025-03-04 20:22:32 +0530

    added layout for SongsTabContent

--- frontend/src/pages/admin/components/SongsTabContent.tsx
+++ frontend/src/pages/admin/components/SongsTabContent.tsx
@@ -1,4 +1,30 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Music } from "lucide-react";
+
 const SongsTabContent = () => {
-  return <div>SongsTabContent</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="flex items-center gap-2">
+              <Music className="size-5 text-emerald-500" />
+              Songs Library
+            </CardTitle>
+            <CardDescription>Manage Your Music Tracks</CardDescription>
+          </div>
+          <Button>+</Button>
+        </div>
+      </CardHeader>
+      <CardContent></CardContent>
+    </Card>
+  );
 };
 export default SongsTabContent;
