8886456b148ce73816787746ac78984b8e208252
adarsh02 <aadarshraazz007@gmail.com>
2025-03-04 15:19:17 +0530

    imported and used Header and DashboardStats

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -1,4 +1,6 @@
 import { useAuthStore } from "@/stores/useAuthStore";
+import Header from "./components/Header";
+import DashboardStats from "./components/DashboardStats";
 
 const AdminPage = () => {
   const { isAdmin, isLoading } = useAuthStore();
@@ -6,7 +8,10 @@ const AdminPage = () => {
   if (!isAdmin && !isLoading) return <div>Unauthorized</div>;
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-zinc-900 via-zinc-900 to-black text-zinc-100 p-8"></div>
+    <div className="min-h-screen bg-gradient-to-b from-zinc-900 via-zinc-900 to-black text-zinc-100 p-8">
+      <Header />
+      <DashboardStats />
+    </div>
   );
 };
 
