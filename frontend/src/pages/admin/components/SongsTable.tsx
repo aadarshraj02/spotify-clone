c5b5d0096496932dce952fd000786e9bfca70c3b
adarsh02 <aadarshraazz007@gmail.com>
2025-03-04 20:38:59 +0530

    added states

--- frontend/src/pages/admin/components/SongsTable.tsx
+++ frontend/src/pages/admin/components/SongsTable.tsx
@@ -1,4 +1,24 @@
+import { useMusicStore } from "@/stores/useMusicStore";
+
 const SongsTable = () => {
+  const { songs, isLoading, error } = useMusicStore();
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center py-8">
+        <div className="text-zinc-400">Loading Songs...</div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center py-8">
+        <div className="text-red-500">{error}</div>
+      </div>
+    );
+  }
+
   return <div>SongsTable</div>;
 };
 export default SongsTable;
