218d5620b5686651b8640fbef1cf6f30463e6f43
adarsh02 <aadarshraazz007@gmail.com>
2025-03-04 15:30:52 +0530

    Made layout in admin; Created Song tab functional component

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -1,6 +1,8 @@
 import { useAuthStore } from "@/stores/useAuthStore";
 import Header from "./components/Header";
 import DashboardStats from "./components/DashboardStats";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Album, Music } from "lucide-react";
 
 const AdminPage = () => {
   const { isAdmin, isLoading } = useAuthStore();
@@ -11,6 +13,27 @@ const AdminPage = () => {
     <div className="min-h-screen bg-gradient-to-b from-zinc-900 via-zinc-900 to-black text-zinc-100 p-8">
       <Header />
       <DashboardStats />
+      <Tabs defaultValue="songs" className="space-y-6">
+        <TabsList className="p-1 bg-zinc-800/50">
+          <TabsTrigger
+            value="songs"
+            className="data-[state=active]:bg-zinc-700"
+          >
+            <Music className="mr-2 size-4" />
+            Songs
+          </TabsTrigger>
+          <TabsTrigger
+            value="albums"
+            className="data-[state=active]:bg-zinc-700"
+          >
+            <Album className="mr-2 size-4" />
+            Album
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="songs">Songs tab content</TabsContent>
+        <TabsContent value="albums">Albums tab content</TabsContent>
+      </Tabs>
     </div>
   );
 };
