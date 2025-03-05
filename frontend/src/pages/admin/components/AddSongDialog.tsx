6c1ddd8f03c116e977bab400f5d4344db64ac98c
adarsh02 <aadarshraazz007@gmail.com>
2025-03-05 11:33:57 +0530

    added layout to upload image and audio file

--- frontend/src/pages/admin/components/AddSongDialog.tsx
+++ frontend/src/pages/admin/components/AddSongDialog.tsx
@@ -1,4 +1,121 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useMusicStore } from "@/stores/useMusicStore";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { Plus, Upload } from "lucide-react";
+import { useRef, useState } from "react";
+
 const AddSongDialog = () => {
-  return <div>AddSongDialog</div>;
+  const { albums } = useMusicStore();
+  const [songDialogOpen, setSongDialogOpen] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [newSong, setNewSong] = useState({
+    title: "",
+    artist: "",
+    album: undefined,
+    duration: 0,
+  });
+
+  const [files, setFiles] = useState<{
+    audio: File | null;
+    image: File | null;
+  }>({
+    audio: null,
+    image: null,
+  });
+
+  const audioInputRef = useRef<HTMLInputElement>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async () => {};
+
+  return (
+    <Dialog open={songDialogOpen} onOpenChange={setSongDialogOpen}>
+      <DialogTrigger asChild>
+        <Button className="bg-emerald-500 hover:bg-emerald-600 text-black">
+          <Plus className="mr-2 size-4" />
+          Add Song
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="bg-zinc-900 border-zinc-700 max-h-[80vh] overflow-auto">
+        <DialogHeader>
+          <DialogTitle>Add New Song</DialogTitle>
+          <DialogDescription>
+            Add a new Song to your music library
+          </DialogDescription>
+        </DialogHeader>
+        <div className="space-y-4 py-4">
+          <input
+            type="file"
+            accept="audio/*"
+            ref={audioInputRef}
+            className="hidden"
+            onChange={(e) =>
+              setFiles((prev) => ({ ...prev, audio: e.target.files![0] }))
+            }
+          />
+          <input
+            type="file"
+            ref={imageInputRef}
+            className="hidden"
+            accept="image/*"
+            onChange={(e) =>
+              setFiles((prev) => ({ ...prev, image: e.target.files![0] }))
+            }
+          />
+          <div
+            className="flex items-center justify-center p-6 border-2 border-dashed border-zinc-700 rounded-lg cursor-pointer"
+            onClick={() => imageInputRef.current?.click()}
+          >
+            <div className="text-center">
+              {files.image ? (
+                <div className="space-y-2">
+                  <div className="text-sm text-emerald-500">
+                    Image Selected:
+                  </div>
+                  <div className="text-sm text-zinc-400">
+                    {files.image.name.slice(0, 20)}
+                  </div>
+                </div>
+              ) : (
+                <>
+                  <div className="p-3 bg-zinc-800 rounded-full inline-block mb-2">
+                    <Upload className="size-6 text-zinc-400" />
+                  </div>
+                  <div className="text-sm text-zinc-400 mb-2">
+                    Upload artwork
+                  </div>
+                  <Button variant={"outline"} size={"sm"} className="text-xs">
+                    Choose file
+                  </Button>
+                </>
+              )}
+            </div>
+          </div>
+
+          <div className="space-y-2">
+            <label htmlFor="" className="text-sm font-medium">
+              Audio File
+            </label>
+            <div className="flex items-center gap-2">
+              <Button
+                variant={"outline"}
+                className="w-full"
+                onClick={() => audioInputRef.current?.click()}
+              >
+                {files.audio ? files.audio.name : "Upload Audio File"}
+              </Button>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 };
 export default AddSongDialog;
