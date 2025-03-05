8f49ca0557b81e48d5a3d01cfa0193e6d8e49554
adarsh02 <aadarshraazz007@gmail.com>
2025-03-05 10:42:43 +0530

    imported and used addSongDialog

--- frontend/src/pages/admin/components/SongsTabContent.tsx
+++ frontend/src/pages/admin/components/SongsTabContent.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/card";
 import { Music } from "lucide-react";
 import SongsTable from "./SongsTable";
+import AddSongDialog from "./AddSongDialog";
 
 const SongsTabContent = () => {
   return (
@@ -21,7 +22,7 @@ const SongsTabContent = () => {
             </CardTitle>
             <CardDescription>Manage Your Music Tracks</CardDescription>
           </div>
-          <Button>+</Button>
+          <AddSongDialog />
         </div>
       </CardHeader>
       <CardContent>
