cfe0b1dd652683453499e98cfa94328d920cf568
adarsh02 <aadarshraazz007@gmail.com>
2025-03-31 12:03:38 +0530

    added layout for image and userOnline

--- frontend/src/pages/chat/ChatPage.tsx
+++ frontend/src/pages/chat/ChatPage.tsx
@@ -2,6 +2,7 @@ import Topbar from "@/components/Topbar";
 import { useChatStore } from "@/stores/useChatStore";
 import { useUser } from "@clerk/clerk-react";
 import { useEffect } from "react";
+import UsersList from "./components/UsersList";
 
 const ChatPage = () => {
   const { user } = useUser();
@@ -22,7 +23,9 @@ const ChatPage = () => {
   return (
     <main className="h-full rounded-lg bg-gradient-to-b from-zinc-800 to-zinc-900 overflow-hidden">
       <Topbar />
-      <div className="grid lg:grid-cols-[300px_1fr] grid-cols-[80px_1fr] h-[calc(100vh-180px)]"></div>
+      <div className="grid lg:grid-cols-[300px_1fr] grid-cols-[80px_1fr] h-[calc(100vh-180px)]">
+        <UsersList />
+      </div>
     </main>
   );
 };
