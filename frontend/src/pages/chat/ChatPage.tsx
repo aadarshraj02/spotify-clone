9a37344945193fad42cd46d0bc39f029426cd6f7
adarsh02 <aadarshraazz007@gmail.com>
2025-03-31 15:37:45 +0530

    added layout for messages

--- frontend/src/pages/chat/ChatPage.tsx
+++ frontend/src/pages/chat/ChatPage.tsx
@@ -3,6 +3,9 @@ import { useChatStore } from "@/stores/useChatStore";
 import { useUser } from "@clerk/clerk-react";
 import { useEffect } from "react";
 import UsersList from "./components/UsersList";
+import ChatHeader from "./components/ChatHeader";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 const ChatPage = () => {
   const { user } = useUser();
@@ -26,7 +29,47 @@ const ChatPage = () => {
       <div className="grid lg:grid-cols-[300px_1fr] grid-cols-[80px_1fr] h-[calc(100vh-180px)]">
         <UsersList />
         <div className="flex flex-col h-full">
-          {selectedUser ? <div></div> : <NoConversation />}
+          {selectedUser ? (
+            <div>
+              <ChatHeader />
+              <ScrollArea>
+                <div className="p-4 space-y-4">
+                  {messages?.map((message) => (
+                    <div
+                      key={message?._id}
+                      className={`flex items-start gap-2 ${
+                        message.senderId === user?.id ? "flex-row-reverse" : ""
+                      }`}
+                    >
+                      <Avatar className="size-8">
+                        <AvatarImage
+                          src={
+                            message.senderId === user?.id
+                              ? user.imageUrl
+                              : selectedUser.imageUrl
+                          }
+                        />
+                      </Avatar>
+                      <div
+                        className={`rounded-lg p-3 max-w-[70%] ${
+                          message.senderId === user?.id
+                            ? "bg-green-500"
+                            : "bg-zinc-800"
+                        }`}
+                      >
+                        <p className="text-sm">{message?.content}</p>
+                        <span className="text-xs text-zinc-300 mt-1 block">
+                          {message?.createdAt}
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </ScrollArea>
+            </div>
+          ) : (
+            <NoConversation />
+          )}
         </div>
       </div>
     </main>
