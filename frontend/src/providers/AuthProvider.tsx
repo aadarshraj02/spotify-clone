865122d2da5ddfe9aa60c1075322ae85cd60cf29
adarsh02 <aadarshraazz007@gmail.com>
2025-03-31 10:40:58 +0530

    called functions for init socket and disconnect socket

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -3,6 +3,7 @@ import { useAuth } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
 import { Loader } from "lucide-react";
 import { useAuthStore } from "@/stores/useAuthStore";
+import { useChatStore } from "@/stores/useChatStore";
 
 const updateApiToken = (token: string | null) => {
   if (token) {
@@ -13,17 +14,21 @@ const updateApiToken = (token: string | null) => {
 };
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const { getToken } = useAuth();
+  const { getToken, userId } = useAuth();
   const [loading, setLoading] = useState(true);
-  const {checkAdminStatus} = useAuthStore()
+  const { checkAdminStatus } = useAuthStore();
+  const { initSocket, disconnectSocket } = useChatStore();
 
   useEffect(() => {
     const initAuth = async () => {
       try {
         const token = await getToken();
         updateApiToken(token);
-        if(token){
-          await checkAdminStatus()
+        if (token) {
+          await checkAdminStatus();
+          if (userId) {
+            initSocket(userId);
+          }
         }
       } catch (error) {
         updateApiToken(null);
@@ -34,7 +39,9 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     };
 
     initAuth();
-  }, [getToken,checkAdminStatus]);
+
+    return () => disconnectSocket();
+  }, [getToken, checkAdminStatus, initSocket, disconnectSocket, userId]);
 
   if (loading) {
     return (
